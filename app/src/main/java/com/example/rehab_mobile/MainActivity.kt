20f6dd3345b38846b43393f0b50bae44ab7d5852
jackytjq <quahshyawei@gmail.com>
2023-02-26 20:09:41 +0800

    add splash screen

--- app/src/main/java/com/example/rehab_mobile/MainActivity.kt
+++ app/src/main/java/com/example/rehab_mobile/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.rehab_mobile
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun loginBtnClick(view: View) {
+
     }
 }
---
+++ app/src/main/java/com/example/rehab_mobile/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.example.rehab_mobile
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.VideoView
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        val videoView = findViewById<VideoView>(R.id.video_view)
+        val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.splash_screen)
+
+        videoView.setVideoURI(videoUri)
+        videoView.setOnPreparedListener {
+            videoView.start()
+        }
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+
+}

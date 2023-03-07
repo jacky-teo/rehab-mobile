d8f370c48fb21dcebc6228f1fec0ce006a052ec0
h-lixuan <39815091+h-lixuan@users.noreply.github.com>
2023-03-07 12:20:27 +0800

    ball info page

--- app/src/main/java/com/example/rehab_mobile/BallInfoActivity.kt
+++ app/src/main/java/com/example/rehab_mobile/BallInfoActivity.kt
@@ -2,10 +2,14 @@ package com.example.rehab_mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class BallInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ball_info)
     }
+
+    fun startGameBtn(view: View) {}
+    fun backBtn(view: View) {}
 }

42452d04ed7bdefa1276b1c15607d44e6e21b551
h-lixuan <39815091+h-lixuan@users.noreply.github.com>
2023-03-21 00:48:31 +0800

    cannot run sia

---
+++ app/src/main/java/com/example/rehab_mobile/RegistrationActivity.kt
@@ -0,0 +1,17 @@
+package com.example.rehab_mobile
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+
+    fun registerSubmitClick(view: View) {}
+    fun cancelBtnClick(view: View) {}
+}
